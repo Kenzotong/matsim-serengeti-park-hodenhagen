74e84b1ebea2cd3245e06107974780fcfd1100e9
Kenzo <709567093tkz@gmail.com>
2021-04-30 16:03:29 +0200

    link events handler

--- src/main/java/org/matsim/analysis/RunAnalysis.java
+++ src/main/java/org/matsim/analysis/RunAnalysis.java
@@ -7,8 +7,12 @@ public class RunAnalysis {
 
     public static void main(String[] args) {
 
+       // EventsManager manager = EventsUtils.createEventsManager();
+       // SimpleEventsHandler handler = new SimpleEventsHandler();
+       // manager.addHandler(handler);
+
         EventsManager manager = EventsUtils.createEventsManager();
-        SimpleEventsHandler handler = new SimpleEventsHandler();
+        LinkEventHandler handler = new LinkEventHandler();
         manager.addHandler(handler);
 
         EventsUtils.readEvents(manager, "D:\\Jetbrain\\matsim-serengeti-park-hodenhagen\\scenarios\\serengeti-park-v1.0\\output\\output-serengeti-park-v1.0-run1\\serengeti-park-v1.0-run1.output_events.xml.gz");
